38fb06731baab1e94edf59692c21129e89a57f7b
Yamil Chamo <yajochakha@gmail.com>
2021-09-28 17:21:50 -0500

    initializing homework 04

---
+++ script.js
@@ -0,0 +1,33 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+//var timer = 0;
+
+
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + "Game Over";
+    
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+    
+  }, 1000);
+}
+
+addEventListener()
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
