9968b6b9df18fac9a9715dca1effbd76dc42d8bb
Yamil Chamo <yajochakha@gmail.com>
2021-10-27 23:00:31 -0500

    Created and Added Second Question and Second set of choices in an On click() fucntion trying to make it work

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
+
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
@@ -14,114 +12,134 @@ var timerDisplay = document.getElementById('timerDisplay');
 var correctAns = document.body.getElementsByClassName('correct');
 var incorrectAns = document.body.getElementsByClassName('incorrect');
 var strtBtn = document.getElementById("strtBtn");
-var timRem = 60;
 var question1 = document.getElementById("firstQuestion");
-var question2 = [
-  {
-    question: "What CSS stands for?",
-    choices: ["Central Space Staion", "Cascading Style Sheet", "Color Style Sheet", "Customer Service and Support"],
-    answer: 1
-  }
-];
-
+var question2 = document.getElementById("secondQuestion");
 var bntClick = document.getElementById("bntClick")
+var timRem = 60;
+// Clicking Start Quiz! starts timer with Game over alert when run out of time.
+function strtQuiz() {
+  var timer = setInterval(function(){
+    timRem--;
+    timerDisplay.innerHTML='00:'+timRem;
+    if (timRem < 0) {
+      clearInterval(timer);
+      alert("Game Over!");
+    }  
+    }, 1000);
+};
 
-// incorrect is defined wrong
-// I need to do the variable inside my function
-// Choices function
-function firstChoice() {
+// Function container of Q1 Choices.
+function q1Choices() {
+  firstChoiceQ1();
+  secChoiceQ1();
+  thrdChoiceQ1();
+  fourthChoiceQ1();
+};
+
+// Q1 Choice functions. Creating the buttons, elements, #Ids and choices with
+// the penalty() function added on wrong choice and giving the next question when correct answer is clicked.
+function firstChoiceQ1() {
   var createBtn = document.createElement("button");
   var textNode = document.createTextNode("HyperTense Markup Language")
   createBtn.appendChild(textNode);
   document.getElementById("firstAns").appendChild(createBtn);
   createBtn.classList.add("incorrect");
-  createBtn.onclick = penQu;
+  createBtn.onclick = penalty;
 };
 
-
-function penQu() {
-  timRem -= 5;
-  console.log("squirrel");
-
-};
-
-// for (var index = 0; index < "Array"(not an actual string just an example).length; --1)
-
-//Heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy Start HEreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee when your back!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//selecting correct answer will take to the next question saying Correct!
-//write a second set of question and answers (basically copy paste previous questions)
-//rinse and repeat
-// m
-
-
-//separate bunt click from penalty function
-
-
-function secChoice() {
-  //var incorrectAns = document.body.getElementsByClassName('incorrect');
+function secChoiceQ1() {
   var createBtn = document.createElement("button");
   var textNode = document.createTextNode("Hotmail")
   createBtn.appendChild(textNode);
   document.getElementById("firstAns").appendChild(createBtn);
   createBtn.classList.add("incorrect");
-  createBtn.onclick = penQu;
-}; 
+  createBtn.onclick = penalty;
+};
 
-
-function thrdChoice() {
+function thrdChoiceQ1() {
   var createBtn = document.createElement("button");
   var textNode = document.createTextNode("HyperText Markup Language")
   createBtn.appendChild(textNode);
   document.getElementById("firstAns").appendChild(createBtn);
   createBtn.classList.add("correct");
-};    
-function fourthChoice() {
+};
+
+function fourthChoiceQ1() {
   var createBtn = document.createElement("button");
   var textNode = document.createTextNode("HyperTech Mockup Language")
   createBtn.appendChild(textNode);
   document.getElementById("firstAns").appendChild(createBtn);
   createBtn.classList.add("incorrect");
-  createBtn.onclick = penQu;
+  createBtn.onclick = penalty;
 };
 
 
 
+// Q2 Choice functions. Creating the buttons, elements, #Ids and choices with
+// the penalty() function added on wrong choice and giving the next question when correct answer is clicked.
+function firstChoiceQ2() {
+  var createBtn = document.createElement("button");
+  var textNode = document.createTextNode("Central Space Staion")
+  createBtn.appendChild(textNode);
+  document.getElementById("secondAns").appendChild(createBtn);
+  createBtn.classList.add("incorrect");
+  createBtn.onclick = penalty;
+};
 
-function strtQuiz() {
-    var timer = setInterval(function(){
-      timRem--;
-      timerDisplay.innerHTML='00:'+timRem;
-      if (timRem < 0) {
-        clearInterval(timer);
-        alert("Game Over!");
-      }  
-      }, 1000);
+function secChoiceQ2() {
+  var createBtn = document.createElement("button");
+  var textNode = document.createTextNode("Cascading Style Sheet")
+  createBtn.appendChild(textNode);
+  document.getElementById("secondAns").appendChild(createBtn);
+  createBtn.classList.add("correct");
+};
+
+function thrdChoiceQ2() {
+  var createBtn = document.createElement("button");
+  var textNode = document.createTextNode("Color Style Sheet")
+  createBtn.appendChild(textNode);
+  document.getElementById("secondAns").appendChild(createBtn);
+  createBtn.classList.add("incorrect");
+  createBtn.onclick = penalty;
+};
+
+function fourthChoiceQ2() {
+  var createBtn = document.createElement("button");
+  var textNode = document.createTextNode("Customer Service and Support")
+  createBtn.appendChild(textNode);
+  document.getElementById("secondAns").appendChild(createBtn);
+  createBtn.classList.add("incorrect");
+  createBtn.onclick = penalty;
 };
 
 
+// Penalty fuction that subtracs 5 secs when wrong answer is clicked
 function penalty() {
-  document.getElementsByClassName('incorrect').addEventListener('click', function() {
-    timRem -= 5;
-    document.getElementById('timerDisplay').innerHTML='00:'+timRem;
-    
-  })
-};
-
-// function Questns() {
-//   // for (var i = 0; i < myQuestions.length; i++) {
-//   var question = question1
-//   $('#container').text (question);
-//   // var options = myQuestions[0].choices;
-//   // document.body.appendChild(document.createElement("br"));
-    
-// }
+  timRem -= 5;
+    console.log("squirrel");
   
-// Append the First question on click
-
-//need to fix your questions and put them individually and If statement for submit and show nex question
+  };
 
 
 
+
+// On click function to make submission of correct answer to display the the Second Question and the second set of choices.
+  $( "#correct" ).click(function() {
+    alert( "Handler for .click() called." );
+    $('#secondQuestion').text("What CSS stands for?");
+    firstChoiceQ2();
+    secChoiceQ2();
+    thrdChoiceQ2();
+    fourthChoiceQ2();
+  });
+
+
+
+  //Heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy Start HEreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee when your back!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//selecting correct answer will take to the next question saying Correct!
+//and making the old questions disappear
+//write a second set of question and answers (basically copy paste previous questions)
+//rinse and repeat
 
 
 
@@ -130,11 +148,15 @@ function penalty() {
 strtBtn.addEventListener("click", function() {
   $('#demo').text("Good Luck!");
   strtBtn.style.display= 'none'
+  $('#firstQuestion').text("What HTML stands for?");
+  
+  
+  
+  
+  
   
   strtQuiz();
-  firstChoice();
-  secChoice();
-  thrdChoice();
-  fourthChoice();
+  q1Choices();
+  
   // penalty();
 });
